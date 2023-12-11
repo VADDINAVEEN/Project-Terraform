136bc0fa21088263bd6a4b54ba0d08542df3da68
Naveen <vaddi.naveen@gmail.com>
2023-12-11 18:11:01 +0530

    files added to git

---
+++ Providers.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAWALGIZMZCWTTFJTL"
+  secret_key = "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"
+}
---
+++ lms-web.sh
@@ -0,0 +1,14 @@
+# #!/bin/bash
+# sudo git clone -b https://github.com/VADDINAVEEN/lms-team-1-08-12.git
+# curl -fsSL https://get.docker.com -o get-docker.sh
+# sudo sh ./get-docker.sh --dry-run
+# curl -fsSL https://get.docker.com -o get-docker.sh
+# sudo sh get-docker.sh
+# Executing docker install script, commit: 7cae5f8b0decc17d6571f9f52eb840fbc13b2737
+# <...>
+# sudo groupadd docker
+# sudo usermod -aG docker $USER
+# newgrp docker
+# cd lms-team-1-praneeth/LMS-FE
+# docker build -t frontend .
+# docker run -dt --name lmsfe -p 80:80 frontend
